37b521bb0c6b9d6cccbb5d11e93d98f026965b7a
MOHIT DONGRE <mohitdongre4235@gmail.com>
2026-09-03 15:34:55 +0530

    variable removed

--- src/main/java/com/zepto/OrderServiceApplication.java
+++ src/main/java/com/zepto/OrderServiceApplication.java
@@ -8,8 +8,6 @@ public class OrderServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
-                int i=10;
-                int j=30;
 	}
 
 }
